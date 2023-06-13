d46f4a847d0c24f596511023eb20e10ed70ecb09
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-06-13 14:08:57 +0200

    Complete Main Screen UI

--- SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthentication/MainScreen.swift
+++ SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthentication/MainScreen.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct MainScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Image("BackgroundScreen")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("Sign Out")
+                            .padding(10)
+                            .foregroundColor(.white)
+                            .background(.white .opacity(0.3))
+                            .clipShape(Capsule())
+                    }
+                    .padding()
+                }
+                Spacer()
+                
+                VStack {
+                    Text("Welcome Here")
+                        .font(.system(size: 42, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                    
+                    Text("Let's grow together")
+                        .font(.title3)
+                        .foregroundColor(.white .opacity(0.3))
+                }
+                .padding(.bottom, 60)
+                
+                Spacer()
+                
+            }
+        }
+        
     }
 }
 
