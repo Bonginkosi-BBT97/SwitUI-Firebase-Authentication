84b6a5cb192ad690259bc949930fd975173801da
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-06-13 13:01:06 +0200

    Initial files

---
+++ SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthenticationApp.swift
+//  SwiftUIFirebaseAuthentication
+//
+//  Created by Bonginkosi Tshabalala on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
