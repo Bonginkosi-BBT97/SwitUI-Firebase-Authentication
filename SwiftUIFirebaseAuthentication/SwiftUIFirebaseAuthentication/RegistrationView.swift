5dd8e00670e384af65cb83dfc8aa335ac3ad6c99
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-06-13 13:57:35 +0200

    Complete Sign Up UI

--- SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthentication/RegistrationView.swift
+++ SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthentication/RegistrationView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("BackgroundScreen")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                VStack {
+                    Text("Sign Up Now")
+                        .font(.system(size: 42,weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                    
+                    Text("Please fill the details and an create account")
+                        .font(.title3)
+                        .foregroundColor(.white.opacity(0.7))
+                }
+                
+                VStack(spacing: 20) {
+                    TextField("Enter Name", text: $name)
+                        .modifier(CustomTextField())
+                    TextField("Enter Email", text: $email)
+                        .modifier(CustomTextField())
+                    
+                    SecureField("Enter Password", text: $password)
+                        .modifier(CustomTextField())
+                }
+                .padding(.horizontal)
+                .padding(.top, 60)
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        Text("Create Account")
+                            
+                    }
+                    .buttonStyle(CustomButton())
+                    
+                    HStack {
+                        Text("Already have an acount?")
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Sign In")
+                                .font(.subheadline)
+                                .foregroundColor(.red .opacity(0.7))
+                        }
+                    }
+                }
+                
+                .padding(.horizontal)
+                Spacer()
+            }
+        }
     }
+
 }
 
 struct RegistrationView_Previews: PreviewProvider {
